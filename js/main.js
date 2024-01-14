30e9008ce91eac7c3b4b3895faf42745e3aa8d9f
aleksandra <grzeczkowska.a@gmail.com>
2024-01-14 18:33:30 +0100

    moved "about me" section to a popup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function showAbout(){
+  Swal.fire({
+    title: "O mnie",
+    text: "Lepiej mi idzie wymyślanie tasiemcowych opowiadań niż ich spisywanie, ale robię, co w mojej mocy.",
+    icon: "question",
+    confirmButtonText: "Zamknij",
+    confirmButtonColor: "#708090"
+  });
+}
